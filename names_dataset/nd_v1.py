f03e6f2b79abc0256f47b005f62bbb70fe88ae53
Nischal Sanil <42136912+nischal-sanil@users.noreply.github.com>
2021-09-07 17:21:28 +0530

    extend namedatasetv1
    can add additional first name and last name to the list.

--- names_dataset/nd_v1.py
+++ names_dataset/nd_v1.py
@@ -3,13 +3,17 @@ import os
 
 class NameDatasetV1:
 
-    def __init__(self):
+    def __init__(self, fn_ext=None, ln_ext=None):
         first_names_filename = os.path.join(os.path.dirname(__file__), 'v1/first_names.all.txt')
         with open(first_names_filename, 'r', errors='ignore', encoding='utf8') as r:
             self.first_names = set(r.read().strip().split('\n'))
         last_names_filename = os.path.join(os.path.dirname(__file__), 'v1/last_names.all.txt')
         with open(last_names_filename, 'r', errors='ignore', encoding='utf8') as r:
             self.last_names = set(r.read().strip().split('\n'))
+        if fn_ext is not None and isinstance(fn_ext,list):
+            for i in fn_ext: self.first_names.add(i.lower())
+        if ln_ext is not None and isinstance(ln_ext,list):
+            for i in ln_ext: self.last_names.add(i.lower())
 
     def search_first_name(self, first_name, use_upper_case=False):
         return self._query(self.first_names, first_name, use_upper_case)
